1b45e0652ff16b94b3eccda3a5cfeb1717169748
trantungzm <trantung20002005@gamil.com>
2026-08-28 14:41:21 +0700

    fix: generate prisma client after install

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
   },
   datasource: {
     url:
-      process.env["DATABASE_URL"] ||
       process.env["TURSO_DATABASE_URL"] ||
+      process.env["DATABASE_URL"] ||
       "file:./prisma/dev.db",
   },
 });
